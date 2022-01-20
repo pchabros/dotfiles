13dc4028c4303f00c4792822260ca1efd9b0bea2
pawelchabros <pawel.chabros@appsilon.com>
2022-01-20 23:28:01 +0100

    Order of completions fixed

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -22,10 +22,33 @@ ts.setup({ ensure_installed = "maintained", highlight = { enable = true } })
 -- completion related settings
 local cmp = require("cmp")
 
+local lspkind_comparator = function(conf)
+  local lsp_types = require('cmp.types').lsp
+  return function(entry1, entry2)
+    if entry1.source.name ~= 'nvim_lsp' then
+      if entry2.source.name == 'nvim_lsp' then
+        return false
+      else
+        return nil
+      end
+    end
+    local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+    local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+
+    local priority1 = conf.kind_priority[kind1] or 0
+    local priority2 = conf.kind_priority[kind2] or 0
+    if priority1 == priority2 then
+      return nil
+    end
+    return priority2 < priority1
+  end
+end
+
+local label_comparator = function(entry1, entry2)
+  return entry1.completion_item.label < entry2.completion_item.label
+end
+
 cmp.setup({
-  sources = {
-    { name = "cmp_tabnine" },
-  },
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
@@ -42,7 +65,41 @@ cmp.setup({
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
-    { name = "buffer" },
+    -- { name = "buffer" },
+  },
+  sorting = {
+    comparators = {
+      lspkind_comparator({
+        kind_priority = {
+          Field = 11,
+          Property = 11,
+          Constant = 10,
+          Keyword = 11,
+          Enum = 10,
+          EnumMember = 10,
+          Event = 10,
+          Function = 10,
+          Method = 10,
+          Operator = 10,
+          Reference = 10,
+          Struct = 10,
+          Variable = 9,
+          File = 8,
+          Folder = 8,
+          Class = 5,
+          Color = 5,
+          Module = 5,
+          Constructor = 1,
+          Interface = 1,
+          Snippet = 0,
+          TypeParameter = 1,
+          Unit = 1,
+          Value = 1,
+          Text = 0,
+        },
+      }),
+      label_comparator,
+    },
   }
 })
 
